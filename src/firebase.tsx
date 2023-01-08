5d0600791f939dcf499177a3d41241ca504ef645
Hardik Mehta <hadzy1802@gmail.com>
2023-01-08 19:10:49 +0530

    do some changes

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGEINGSEND_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_IF,
+  apiKey: process.env.REACT_APP_VERCEL_API_KEY,
+  authDomain: process.env.REACT_APP_VERCEL_DOMAIN,
+  projectId: process.env.REACT_APP_VERCEL_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_VERCEL_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_VERCEL_MESSAGEINGSEND_ID,
+  appId: process.env.REACT_APP_VERCEL_APP_ID,
+  measurementId: process.env.REACT_APP_VERCEL_MEASUREMENT_IF,
 };
 
 const app = initializeApp(firebaseConfig);
