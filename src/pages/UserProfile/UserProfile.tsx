eff50e368538aedb4fac89884ff066eca11350c5
Hardik Mehta <hadzy1802@gmail.com>
2023-02-18 15:59:02 +0530

    add like functionality in app

---
+++ src/pages/UserProfile/UserProfile.tsx
@@ -0,0 +1,101 @@
+import { Card } from "antd";
+import Typography from "antd/es/typography/Typography";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./UserProfile.scss";
+
+const UserProfile = () => {
+  const { id } = useParams();
+  useEffect(() => {
+    console.log("Id", id);
+  }, []);
+
+  return (
+    <>
+      <section className="h-screen min-h-screen bg-transparent flex flex-col w-full">
+        <div className="p-3 border-b-2 border-cyan-200">
+          <div className="flex justify-evenly ">
+            <img
+              src="https://i1.wp.com/cdn.auth0.com/avatars/ad.png?ssl=1"
+              alt="UserLog..."
+              className="rounded-full"
+            />
+            <div>
+              <Typography className="text-white p-2 font-family">
+                UserName
+              </Typography>
+              <Typography className="text-white p-2 font-family">
+                Some data Some data Some data{" "}
+              </Typography>
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div className="p-3 ">
+          <section className="overflow-hidden text-gray-700 ">
+            <div className="container   mx-auto pt-5 ">
+              <div className="flex flex-wrap -m-1 md:-m-2">
+                <div className="flex flex-wrap w-1/3">
+                  <div className="w-full p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      className="block object-cover object-center w-full h-full rounded-lg"
+                      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-wrap w-1/3">
+                  <div className="w-full p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      className="block object-cover object-center w-full h-full rounded-lg"
+                      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-wrap w-1/3">
+                  <div className="w-full p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      className="block object-cover object-center w-full h-full rounded-lg"
+                      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-wrap w-1/3">
+                  <div className="w-full p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      className="block object-cover object-center w-full h-full rounded-lg"
+                      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-wrap w-1/3">
+                  <div className="w-full p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      className="block object-cover object-center w-full h-full rounded-lg"
+                      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-wrap w-1/3">
+                  <div className="w-full p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      className="block object-cover object-center w-full h-full rounded-lg"
+                      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default UserProfile;
--- src/routes/RouteCom.tsx
+++ src/routes/RouteCom.tsx
@@ -1,20 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { PostPageMain, Profile } from "../pages/index";
-import { BottomNavigation } from "../components/index";
 import PrivateRoute from "./PrivateRoute";
-import DesktopNavigation from "../components/DeskTopNavigation/DesktopNavigation";
 import LayOut from "../pages/Layout";
+import UserProfile from "../pages/UserProfile/UserProfile";
 
 const RouteCom = () => {
   return (
     <>
-      {/* <DesktopNavigation />
-      <BottomNavigation /> */}
       <Routes>
         <Route path="/" element={<PrivateRoute />}>
           <Route path="/" element={<LayOut />}>
             <Route path="/" element={<PostPageMain />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/:id" element={<UserProfile />} />
           </Route>
         </Route>
       </Routes>
