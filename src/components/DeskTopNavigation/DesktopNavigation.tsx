83b4802a61c623fb849ec51ae6c63cb7d0778fbd
Hardik Mehta <hadzy1802@gmail.com>
2023-02-20 16:44:05 +0530

    dom some changes

--- src/components/DeskTopNavigation/DesktopNavigation.tsx
+++ src/components/DeskTopNavigation/DesktopNavigation.tsx
@@ -7,7 +7,7 @@ import { HomeFilled, UserOutlined, UploadOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import PostModal from "../PostUploadModal/Modal";
 
-
+import "./DeskTopNavigation.scss";
 const items: MenuProps["items"] = [
   {
     label: <Link to="/">Home</Link>,
@@ -37,6 +37,7 @@ const DesktopNavigation: React.FC<Props> = ({ collapsed, setCollapsed }) => {
         collapsible
         collapsed={collapsed}
         onCollapse={(value) => setCollapsed(value)}
+        className="aside-bg-color"
       >
         <Menu
           theme="dark"
